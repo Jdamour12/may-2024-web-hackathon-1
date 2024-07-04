129dee39b85042950edfd91839d6c526d2000292
Jdamour12 <jeandamourkubwimana0@gmail.com>
2024-07-04 13:24:46 +0200

    Updating HTML, CSS and Javascript files of my portfolio

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var tab_links = document.getElementsByClassName("tab-links");
 var tab_contents=document.getElementsByClassName("tab-contents");
 
+
+var side_menu=document.getElementById("side-menu");
+
 function openTab(tab_name) {
     for(tab_link of tab_links) {
         tab_link.classList.remove("active-link");
@@ -12,4 +15,13 @@ function openTab(tab_name) {
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tab_name).classList.add("active-tab");
+}
+
+
+function openMenu(){
+    side_menu.style.right="0";
+}
+
+function closeMenu(){
+    side_menu.style.right="-200px";
 }
