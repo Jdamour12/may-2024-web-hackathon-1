bc34c60b136a6ba99ea4f82ba5e18ec087682d07
Jdamour12 <jeandamourkubwimana0@gmail.com>
2024-07-04 15:16:22 +0200

    Further Updates on HTML and CSS files of my portfolio

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ var tab_links = document.getElementsByClassName("tab-links");
 var tab_contents=document.getElementsByClassName("tab-contents");
 
 
-var side_menu=document.getElementById("side-menu");
-
 function openTab(tab_name) {
     for(tab_link of tab_links) {
         tab_link.classList.remove("active-link");
@@ -17,6 +15,9 @@ function openTab(tab_name) {
     document.getElementById(tab_name).classList.add("active-tab");
 }
 
+// Javascript for small screens
+
+var side_menu=document.getElementById("side-menu");
 
 function openMenu(){
     side_menu.style.right="0";
@@ -25,3 +26,23 @@ function openMenu(){
 function closeMenu(){
     side_menu.style.right="-200px";
 }
+
+// Javascript for linking my portfolio with google spreadsheets
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw_Q_ZPY7XhNhLgtV_HhiWUq5dsPqvKo4ZKEei4-Y5VABCxQN1Wf6hd1asV-2_cZDrpeA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg=document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Thank you for your message! I'll get back to you shortly."
+        setTimeout(function(){
+            msg.innerHTML=""
+        }, 10000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
